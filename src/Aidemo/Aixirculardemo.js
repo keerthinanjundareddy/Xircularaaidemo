c84ef12510f1c19505b4736323abcb284e216a13
keerthinanjundareddy <keerthana@apprikart.com>
2023-08-23 16:38:44 +0530

    message

--- src/Aidemo/Aixirculardemo.js
+++ src/Aidemo/Aixirculardemo.js
@@ -19,6 +19,7 @@ function Aixirculardemo() {
   }, []);
 
   const handleCardClick = (url) => {
+
     if (url) {
       window.open(url, '_blank');
     }
@@ -29,18 +30,23 @@ function Aixirculardemo() {
       {pricingData && pricingData.attributes && (
         <>
           <div style={{ textAlign: "center" }}>
-            <h2 style={{ color: 'red' }}>{pricingData.attributes.title}</h2>
-            <p style={{ color: '#0C0C0C' }}>
-              {pricingData.attributes.description_1}
-            </p>
+            <h2 style={{ color: 'grey',textTransform: 'uppercase' }}>{pricingData.attributes.title}</h2>
+          
           </div>
           <div className='pricing-full-page-container'>
             <div className='pricing-flexbox-container'>
               {pricingData.attributes.website_cards.data.map((item, index) => {
+                                const imageUrlone = baseUrl + (item.attributes.media_list?.data?.[0]?.attributes?.url || '');
                 return (
-                  <div key={index} className='pricing-item-section' style={{ backgroundColor: item.attributes.colour_code, borderColor: item.attributes.colour_code }}>
-                    <div style={{ color: item.attributes.title_color_code, paddingTop: "10px", paddingBottom: "10px" }}>{item.attributes.title}</div>
-                    <div style={{ color: item.attributes.description_color_code }}>{item.attributes.description}</div>
+                  <div key={index} className='pricing-item-section' style={{ backgroundColor: item.attributes.colour_code, borderColor: item.attributes.colour_code,cursor:"pointer",alignItems:"center",justifyContent:"center",position:"relative" }} onClick={() => handleCardClick(item.attributes.url)} >
+                     <img src={imageUrlone} style={{width:"50px",height:"50px",objectFit:"cover"}} />
+                    <div style={{ color: item.attributes.title_color_code, paddingTop: "10px", paddingBottom: "10px" ,}}>{item.attributes.title}</div>
+                    <div style={{position:"absolute",marginTop:"30px",
+    top: "100%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    textTransform: "uppercase",}} > {item.attributes.subtitle}</div>
+                    {/* style={{position:"absolute",marginTop:"30px",marginLeft:"80px" ,textTransform: 'uppercase'}} */}
                   </div>
                 );
               })}
