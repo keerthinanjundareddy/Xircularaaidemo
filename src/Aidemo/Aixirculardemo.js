2c73d36786ad3f1161aa9835a3f6702221a0a1ea
keerthinanjundareddy <keerthana@apprikart.com>
2023-08-28 14:24:12 +0530

    me

--- src/Aidemo/Aixirculardemo.js
+++ src/Aidemo/Aixirculardemo.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './aixircular.css';
+import xircularone from '../Assets/Circular logo.png'
 
 function Aixirculardemo() {
   const [pricingData, setPricingData] = useState({});
@@ -8,7 +9,7 @@ function Aixirculardemo() {
 
   useEffect(() => {
     axios
-      .get(`${baseUrl}/api/website-services/2?populate[website_cards][populate][media_list][fields]=url&populate[website_cards][populate][media_list][fields]=alternativeText`)
+      .get(`${baseUrl}/api/website-services/2?[populate][media][fields]=url&populate[website_cards][populate][media_list][fields]=url&[populate][media_list][fields]=alternativeText&populate[website_cards][populate][media_list][fields]=alternativeText`)
       .then((response) => {
         console.log("API Response:", response.data);
         setPricingData(response.data.data);
@@ -26,11 +27,19 @@ function Aixirculardemo() {
   };
 
   return (
+    // src={`${baseUrl}${item.attributes.media_1?.data?.[0]?.attributes?.url}`}
     <div>
+     
       {pricingData && pricingData.attributes && (
         <>
+         <div className='navbar-section'>
+     <div style={{width:"100px",height:"80px"}}>
+      <img  src={`${baseUrl}${ pricingData.attributes.media?.data?.[0]?.attributes?.url}`} style={{width:"100%",height:"100%",objectFit:"contain" }}/>
+      {/* <img src={imageUrltwo} style={{width:"50px",height:"50px",objectFit:"cover"}} /> */}
+     </div>
+      </div>
           <div style={{ textAlign: "center" }}>
-            <h2 style={{ color: 'Black',textTransform: 'uppercase' }}>{pricingData.attributes.title}</h2>
+            <h2 style={{ color: 'Black',textTransform: 'uppercase',paddingTop:"110px" }}>{pricingData.attributes.title}</h2>
           
           </div>
           <div className='pricing-full-page-container'>
@@ -40,7 +49,7 @@ function Aixirculardemo() {
                 return (
                   <div key={index} className='pricing-item-section' style={{ backgroundColor: item.attributes.colour_code, borderColor: item.attributes.colour_code,cursor:"pointer",alignItems:"center",justifyContent:"center",position:"relative" }} onClick={() => handleCardClick(item.attributes.url)} >
                      <img src={imageUrlone} style={{width:"50px",height:"50px",objectFit:"cover"}} />
-                    <div style={{ color: item.attributes.title_color_code, paddingTop: "10px", paddingBottom: "10px" ,}}>{item.attributes.title}</div>
+                    <div style={{ color: item.attributes.title_color_code, paddingTop: "10px", paddingBottom: "10px" }}>{item.attributes.title}</div>
                     <div style={{position:"absolute",marginTop:"30px",
     top: "100%",
     left: "50%",
