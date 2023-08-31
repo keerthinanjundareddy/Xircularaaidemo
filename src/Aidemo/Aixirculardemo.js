126982070312786445d6cb64803d3e3404e021ac
keerthinanjundareddy <keerthana@apprikart.com>
2023-08-31 09:43:31 +0530

    app

--- src/Aidemo/Aixirculardemo.js
+++ src/Aidemo/Aixirculardemo.js
@@ -12,6 +12,7 @@ function Aixirculardemo() {
       .get(`${baseUrl}/api/website-services/2?[populate][media][fields]=url&populate[website_cards][populate][media_list][fields]=url&[populate][media_list][fields]=alternativeText&populate[website_cards][populate][media_list][fields]=alternativeText`)
       .then((response) => {
         console.log("API Response:", response.data);
+        // console.log("color",)
         setPricingData(response.data.data);
       })
       .catch((error) => {
@@ -48,9 +49,10 @@ function Aixirculardemo() {
               {pricingData.attributes.website_cards.data.map((item, index) => {
                                 const imageUrlone = baseUrl + (item.attributes.media_list?.data?.[0]?.attributes?.url || '');
                 return (
-                  <div key={index} className='pricing-item-section' style={{ backgroundColor: item.attributes.colour_code ,borderColor: item.attributes.colour_code ,cursor:"pointer",position:"relative"}} onClick={() => handleCardClick(item.attributes.url)} >
+                  <div key={index} className='pricing-item-section' style={{ backgroundColor:  item.attributes.colour_code   ,borderColor: item.attributes.colour_code ,cursor:"pointer",position:"relative"}} onClick={() => handleCardClick(item.attributes.url)} >
                      <img src={imageUrlone} style={{width:"60px",height:"60px",objectFit:"cover"}} />
                     <div style={{ color: item.attributes.title_color_code, paddingTop: "10px", paddingBottom: "10px" }}>{item.attributes.title}</div>
+                       <div style={{color: item.attributes.description_color_code}}> {item.attributes.description}</div> 
                     <div style={{position:"absolute",marginTop:"30px",
     top: "100%",
     left: "50%",
